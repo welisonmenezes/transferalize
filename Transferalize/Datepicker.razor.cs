f3ef672f63468948a6fe2f4b60b8fea66cd87edd
welisonmenezes <welisonmenezes@hotmail.com>
2020-03-23 19:23:48 -0300

    Start the aplication

---
+++ Transferalize/Datepicker.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Transferalize.Components
+{
+    public class DatepickerBase : ComponentBase
+    {
+        public ElementReference DatepickerContainer;
+
+        [Inject] protected IJSRuntime JSInterop { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSInterop.InvokeAsync<string>("console.log", DatepickerContainer);
+                await JSInterop.InvokeAsync<string>("RunPickdate", DatepickerContainer);
+            }
+        }
+    }
+}
---
+++ Transferalize/wwwroot/js/app.js
@@ -0,0 +1,7 @@
+﻿RunPickdate = function (DatepickerContainer) {
+    alert(DatepickerContainer)
+    var datepicker = DatepickerContainer.querySelector('input');
+    if (datepicker.length) {
+        console.log(datepicker);
+    }
+}
